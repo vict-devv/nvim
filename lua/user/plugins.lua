df83dff4af3e6647d327db2a96db6698e27a54da
Victim <victor.devv@gmail.com>
2022-07-10 18:08:23 -0300

    Add AutoSave

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -93,6 +93,9 @@ return packer.startup(function(use)
     commit = "518e27589c0463af15463c9d675c65e464efc2fe",
   }
 
+  -- AutoSave
+  use "Pocco81/AutoSave.nvim"
+
   -- Git
   use { "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" }
 
