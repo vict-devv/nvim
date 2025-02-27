1e29dd40ee55587f84381dcb2df3ddf5b0220a97
Victor F. <victor.id1993@gmail.com>
2025-02-27 17:13:05 -0500

    rename surrounds.lua
    fix mistake between gitsigns and surround code

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,61 +1,75 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup(require("gitsigns").setup({
-			on_attach = function(bufnr)
-				local gitsigns = require("gitsigns")
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup({
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
 
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
 
-				-- Navigation
-				map("n", "]c", function()
-					if vim.wo.diff then
-						vim.cmd.normal({ "]c", bang = true })
-					else
-						gitsigns.nav_hunk("next")
-					end
-				end)
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gitsigns.nav_hunk("next")
+          end
+        end)
 
-				map("n", "[c", function()
-					if vim.wo.diff then
-						vim.cmd.normal({ "[c", bang = true })
-					else
-						gitsigns.nav_hunk("prev")
-					end
-				end)
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gitsigns.nav_hunk("prev")
+          end
+        end)
 
-				-- Actions
-				map("n", "<leader>hs", gitsigns.stage_hunk)
-				map("n", "<leader>hr", gitsigns.reset_hunk)
+        -- Actions
+        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "stage hunk", noremap = true, silent = true })
+        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "reset hunk", noremap = true, silent = true })
 
-				map("v", "<leader>hs", function()
-					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
+        map("v", "<leader>hs", function()
+          gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "stage hunk, v mode", noremap = true, silent = true })
 
-				map("v", "<leader>hr", function()
-					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
+        map("v", "<leader>hr", function()
+          gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "reset hunk, v mode", noremap = true, silent = true })
 
-				map("n", "<leader>hS", gitsigns.stage_buffer)
-				map("n", "<leader>hR", gitsigns.reset_buffer)
-				map("n", "<leader>hp", gitsigns.preview_hunk)
-				map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+        map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "stage buffer", noremap = true, silent = true })
+        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "reset buffer", noremap = true, silent = true })
+        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview hunk", noremap = true, silent = true })
+        map(
+          "n",
+          "<leader>hi",
+          gitsigns.preview_hunk_inline,
+          { desc = "preview hunk inline", noremap = true, silent = true }
+        )
 
-				map("n", "<leader>hb", function()
-					gitsigns.blame_line({ full = true })
-				end)
+        map("n", "<leader>hb", function()
+          gitsigns.blame_line({ full = true })
+        end, { desc = "blame line", noremap = true, silent = true })
 
-				-- Toggles
-				map("n", "<leader>hl", gitsigns.toggle_current_line_blame)
-				map("n", "<leader>hw", gitsigns.toggle_word_diff)
+        map("n", "<leader>hd", gitsigns.diffthis, { desc = "diff", noremap = true, silent = true })
 
-				-- Text object
-				map({ "o", "x" }, "ih", gitsigns.select_hunk)
-			end,
-		}))
-	end,
+        map("n", "<leader>hD", function()
+          gitsigns.diffthis("~")
+        end, { desc = "diff ~", noremap = true, silent = true })
+
+        -- Toggles
+        map(
+          "n",
+          "<leader>ht",
+          gitsigns.toggle_current_line_blame,
+          { desc = "toggle line blame", noremap = true, silent = true }
+        )
+        -- Text object
+        map({ "o", "x" }, "ih", gitsigns.select_hunk)
+      end,
+    })
+  end,
 }
